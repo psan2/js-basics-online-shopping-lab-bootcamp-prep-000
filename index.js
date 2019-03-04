3124f10f7ee1382606edc9aafe394b9d3ca81070
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-04 20:25:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,55 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {itemName: item, itemPrice: getRandomPrice()}
+  cart.push(newItem)
+  return `${item} has been added to your cart.`
+}
+
+function getRandomPrice(){
+  var min = 0
+  var max = 100
+  return Math.floor(Math.random() * (max-min)) + min
 }
 
 function viewCart() {
-  // write your code here
+  var i
+  var cartList = []
+  if (cart.length === 0){
+    return "Your shopping cart is empty."
+  }
+  for (i=0; i<cart.length; i++){
+      cartList.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+
+//    else if (cart.length === 2){
+//      cartList.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+//      return `In your cart, you have ${cartList}.`
+    }
+  if (cartList.length === 1){
+    return `In your cart, you have ${cartList}.`
+  }
+  else if (cartList.length === 2){
+    return `In your cart, you have ${cartList[0]}, and ${cartList[1]}.`
+  }
+  else{
+    return `In your cart, you have ${cartList.slice(0,cartList.length-1).join(', ')}, and ${cartList[cartList.length-1]}.`
+  }
 }
 
 function total() {
-  // write your code here
+  var total = cart.reduce((a,{itemPrice}) => a + itemPrice, 0)
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var inCart = cart.some(x => item === cart.itemName)
+  if (inCart = false){
+    return "That item is not in your cart."
+  }
+  else {
+    delete cart[item]
+    return cart
+  }
 }
 
 function placeOrder(cardNumber) {
